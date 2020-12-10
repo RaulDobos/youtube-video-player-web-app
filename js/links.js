98d6b67c164268ee8e79c80afaf334373c60644f
RaulDobos <raulpriam@yahoo.com>
2020-12-10 22:17:34 +0200

    added getLinks and deleteLink

--- js/links.js
+++ js/links.js
@@ -4,9 +4,12 @@ window.YoutubeVideoPlayer = {
 
     createLink: function(){
         let linkValue = $("#link-input").val();
+        let titleValue = $('#title-input').val();
         let timeValue = $("#time-input").val();
+
         var requestBody = {
             link: linkValue,
+            title: titleValue,
             time: timeValue
         };
 
@@ -21,12 +24,71 @@ window.YoutubeVideoPlayer = {
         });
     },
 
+    deleteLink: function (id) {
+        $.ajax({
+            url: YoutubeVideoPlayer.API_URL + '?id=' + id,
+            method: 'DELETE'
+        }).done(function () {
+            YoutubeVideoPlayer.getLinks();
+        })
+    },
+
+    getLinks: function (){
+        $.ajax({
+            url: YoutubeVideoPlayer.API_URL,
+        }).done(function (response) {
+            console.log(response);
+            YoutubeVideoPlayer.displayLinks(response.content);
+        });
+    },
+
+    displayLinks: function(links){
+        let rowsHtml = '';
+
+        links.forEach(link => rowsHtml += YoutubeVideoPlayer.getLinkRowHtml(link));
+
+        $('#links-table tbody').html(rowsHtml);
+    },
+
+    getLinkRowHtml: function (LINK){
+
+        let newLink = LINK.link.replace('watch?v=', 'embed/');
+
+        return `<tr>
+                <td>   
+                        <iframe id="ytplayer" type="text/html" width="420" height="315"
+                            src=${newLink}>
+                        </iframe>
+                    
+                </td>
+
+                <td>
+                    <a href="#" class="remove-link" data-id=${LINK.id}>
+                        <i class="fas fa-trash"></i>
+                    </a>
+                </td>
+
+            </tr>`
+    },
+
     bindEvents: function () {
-        $("add-link-form").submit(function () {
+        $("add-link-form").submit(function (event) {
+            event.preventDefault();
             YoutubeVideoPlayer.createLink();
         });
+
+        $('#links-table tbody').delegate('.remove-link', 'click', function(event){
+            event.preventDefault();
+
+            let id = $(this).data('id');
+
+            YoutubeVideoPlayer.deleteLink(id);
+
+        })
     }
 
 };
+
+YoutubeVideoPlayer.getLinks();
 
 YoutubeVideoPlayer.bindEvents();
